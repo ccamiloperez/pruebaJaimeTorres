9fbc46d61e2ca6a55d21e19d4b0827510b2067a8
ASUS <ASUS@DESKTOP-VQT6LMI>
2020-11-17 20:09:02 -0500

    primera version. prueba git.

---
+++ AutoresLibrosJPA/src/main/java/com/udec/autoreslibrosjpa/AutoresLibrosJpaApplication.java
@@ -0,0 +1,13 @@
+package com.udec.autoreslibrosjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoresLibrosJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoresLibrosJpaApplication.class, args);
+	}
+
+}
