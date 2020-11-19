7e307213071262f668f7d96130f316faafb0eb58
ASUS <ASUS@DESKTOP-VQT6LMI>
2020-11-19 16:51:30 -0500

    Cambios para despliegue en tomcat externo.

--- AutoresLibrosJPA/src/main/java/com/udec/autoreslibrosjpa/AutoresLibrosJpaApplication.java
+++ AutoresLibrosJPA/src/main/java/com/udec/autoreslibrosjpa/AutoresLibrosJpaApplication.java
@@ -2,12 +2,19 @@ package com.udec.autoreslibrosjpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AutoresLibrosJpaApplication {
+public class AutoresLibrosJpaApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AutoresLibrosJpaApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AutoresLibrosJpaApplication.class);
 	}
 
 }
